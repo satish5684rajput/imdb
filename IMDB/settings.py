a2960ff4f2d93b53b077c1de8575b88da4eced38
satish5684rajput <satish5684rajput@gmail.com>
2018-02-08 15:20:14 +0530

    changed setting file

--- IMDB/settings.py
+++ IMDB/settings.py
@@ -89,10 +89,23 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 
-STATIC_PATH = os.path.join(BASE_DIR,'static')
+# STATIC_PATH = os.path.join(BASE_DIR,'static')
 
-STATIC_URL = '/static/' # You may find this is already defined as such.
+# STATIC_URL = '/static/' # You may find this is already defined as such.
 
+# STATICFILES_DIRS = (
+#     STATIC_PATH,
+# )
+
+# for heroku static files
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    STATIC_PATH,
+    os.path.join(PROJECT_ROOT, 'static'),
 )
